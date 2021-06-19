202686977a3843aad06841cb1ee5a96693232c8a
Damola Adekoya <adekoyafelix@gmail.com>
2021-06-19 18:26:08 +0100

    Initial Commit

---
+++ StartUpClub/StartUpClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartUpClubApp.swift
+//  StartUpClub
+//
+//  Created by Damola Adekoya on 19/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct StartUpClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
